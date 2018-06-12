2e517213f49a23b726c3b8904970ef02c503d5ba
Anthony Bonello <ab@anthonybonello.co.uk>
2018-06-12 16:00:54 +0000

    Add implemented.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template  #, redirect, request, url_for, session
+from flask import Flask, render_template, request, redirect, url_for #, session
 from flask_pymongo import PyMongo
 # from bson.objectid import ObjectId
 # import bcrypt
@@ -9,23 +9,60 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = getDbName()
 app.config["MONGO_URI"] = getURI()
+collection = getCollection()
 
 mongo = PyMongo(app)
 
 # R     -----------------------
 @app.route("/")
 @app.route("/get_composers")
-def get_tasks():
+def get_composers():
     return render_template("composers.html", composers=mongo.db.composers.find())
     
 # C     -----------------------
 @app.route("/add_composer")
 def add_composer():
-    return "This will render the 'addcomposer.html'"
+    # return "This will render the 'addcomposer.html'"
+    return render_template("addcomposer.html")
+
+@app.route("/add_composer_test")
+def add_composer_test():
+    # return "This will render the 'addcomposer.html'"
+    return render_template("addcomposertest.html")
+    
+def formatNumber(n):
+    if len(n) == 1:
+        return "0{}".format(n)
+    else:
+        return n
     
 @app.route("/insert_composer", methods=["POST"])
 def insert_composer():
-    return "This will only work with a POST. Used to create a new composer entry."
+    # return "This will only work with a POST. Used to create a new composer entry."
+    # composers = mongo.db.composers
+    # composers.insert_one(request.form.to_dict())
+    # print("What arrives from FORM:")
+    # print(request.form)
+    dataFromForm = request.form.to_dict()
+    # print("Turned into a DICTIONARY:")
+    # print(dataFromForm)
+    
+    data = {}
+    data['first_name'] = dataFromForm['first_name']
+    data['last_name'] = dataFromForm['last_name']
+    # dob = "{}/{}/{}".format(formatNumber(dataFromForm['dob-date']), formatNumber(dataFromForm['dob-month']), dataFromForm['dob-year'])
+    # dod = "{}/{}/{}".format(formatNumber(dataFromForm['dod-date']), formatNumber(dataFromForm['dod-month']), dataFromForm['dod-year'])
+    data['dob'] = "{}/{}/{}".format(formatNumber(dataFromForm['dob-date']), formatNumber(dataFromForm['dob-month']), dataFromForm['dob-year'])
+    data['dod'] = "{}/{}/{}".format(formatNumber(dataFromForm['dod-date']), formatNumber(dataFromForm['dod-month']), dataFromForm['dod-year'])
+    data['period'] = dataFromForm['period']
+    data['nationality'] = dataFromForm['nationality']
+    # print("This is DATA:")
+    # print(data)
+    composers = mongo.db.composers
+    composers.insert_one(data)
+    
+    return redirect(url_for("get_composers"))
+    
 
 # U     -----------------------
 @app.route("/edit_composer/<composer_id>")
